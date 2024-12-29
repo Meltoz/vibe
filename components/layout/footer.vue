8916b1aad85b6424fd44858a2a7414120d0abe71
Meltoz <meltoz@outlook.fr>
2024-12-29 19:26:39 +0100

    footer

--- components/layout/footer.vue
+++ components/layout/footer.vue
@@ -1,5 +1,38 @@
 <template>
-  <footer class="bg-gray-800 text-white p-4">
-    <p>&copy; 2021 - Tous droits réservés</p>
+  <footer class="bg-gray-600 text-white">
+    <section class="grid grid-cols-4 mx-10 justify-items-center pt-10">
+
+      <div>
+        <h5 class="font-semibold">Réseaux Sociaux</h5>
+        <ul class="mt-3 px-3">
+          <li>Discord</li>
+          <li>Instagram</li>
+          <li>Facebook</li>
+          <li>Tiktok</li>
+        </ul>
+      </div>
+      <div>
+        <h5 class="font-semibold">Mentions Légales</h5>
+        <ul class="mt-3 px-3">
+          <li>Politique de confidentialité</li>
+          <li>Gestion des cookies</li>
+          <li>CGVU</li>
+        </ul>
+      </div>
+      <div>
+        <h5 class="font-semibold">Thématiques</h5>
+        <ul class="mt-3 px-3">
+          <li><NuxtLink to="/billetterie">Billetterie</NuxtLink></li>
+          <li><NuxtLink to="/programmation">Programmation</NuxtLink></li>
+          <li>Location</li>
+        </ul>
+      </div>
+      <div>
+        <h5 class="font-semibold">Contact</h5>
+        <ul class="px-3 mt-3"><li>contact-festival@vib.com</li></ul>
+      </div>
+    </section>
   </footer>
 </template>
+<script setup lang="ts">
+</script>
--- components/layout/nav.vue
+++ components/layout/nav.vue
@@ -1,17 +1,39 @@
 <template>
-  <nav class="flex justify-evenly h-16 items-center">
-    <NuxtLink to="/">
-      <img src="/imgs/vib.svg" class="h-12"/>
-    </NuxtLink>
-    <NuxtLink v-for="nav in navs" :key="nav.name" :to="nav.to" class="text-3xl rounded-xl bg-pink-300 px-2 pb-1.5 tracking-wide">
-      {{ nav.name }}
-    </NuxtLink>
+  <nav>
+    <div class="container">
+      <ul class="hidden md:flex justify-evenly h-16 items-center">
+        <li>
+          <NuxtLink to="/">
+            <img src="/imgs/vib.svg" class="h-12"/>
+          </NuxtLink>
+        </li>
+        <li v-for="nav in navs" :key="nav.name">
+          <NuxtLink :to="nav.to" class="text-3xl rounded-xl bg-pink-300 px-2 pb-1.5 tracking-wide hover:bg-pink-200">
+            {{ nav.name }}
+          </NuxtLink>
+        </li>
+      </ul>
+      <div class="md:hidden flex justify-between mx-5 items-center" @click="isOpen = !isOpen" >
+        <NuxtLink to="/">
+          <img src="/imgs/vib.svg" class="h-6"/>
+        </NuxtLink>
+        <Menu :size="36"/>
+      </div>
+    </div>
 
-    <ul></ul>
+
+    <ul class="md:hidden" v-if="isOpen">
+      <li v-for="nav in navs" :key="nav.name">
+        <NuxtLink :to="nav.to" class="block px-6 py-2">{{ nav.name }}</NuxtLink>
+      </li>
+    </ul>
   </nav>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import { Menu } from 'lucide-vue-next';
+
 const navs = [{
   name: 'Billetterie',
   to: '/billetterie'
@@ -21,6 +43,7 @@ const navs = [{
 }, {
   name: 'A propos',
   to: '/about'
-}]
+}];
+const isOpen = ref(false);
 
 </script>
